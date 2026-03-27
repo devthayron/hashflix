830ac456cade401963a9af1507329cfdf11cdf83
devthayron <devthayronhiglander@gmail.com>
2026-03-26 22:54:07 -0300

    Criando o modelo Filme no banco de dados

---
+++ filme/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+
+# choices → define opções fixas para um campo
+# formato: (valor_salvo_no_banco, valor_exibido_para_usuario)
+
+LISTA_CATEGORIAS = (
+    ('ANALISES','Análises'),
+    ('PROGRAMACAO','Programação'),
+    ('APRESENTACAO','Apresentação'),
+    ('OUTROS','Outros'),
+)
+class Filme(models.Model):
+    titulo = models.CharField(max_length=50)
+    thumb = models.ImageField(upload_to='thumb_filmes')
+    descricao = models.TextField()
+    categoria = models.CharField(max_length=20,choices=LISTA_CATEGORIAS)
+    visualizacoes = models.IntegerField(default=0)
+    data_criacao = models.DateTimeField(default=timezone.now)  # Define a data na criação e permite alteração manual depois
+
