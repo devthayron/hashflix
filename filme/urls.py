66b8d93aa62c40fcc5aa8c25bfb4adc51e03bab4
devthayron <devthayronhiglander@gmail.com>
2026-04-08 22:47:30 -0300

    exibindo lista de filmes no homefilmes.html com FBV

--- filme/urls.py
+++ filme/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import homepage
+from .views import homepage,homefilmes
 
 urlpatterns = [
     path('', homepage),
+    path('filmes/',homefilmes)
 ]
--- filme/views.py
+++ filme/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Filme
 
 def homepage(request):
     return render(request, 'homepage.html')
+
+def homefilmes(request):
+    context = {
+        'lista_filmes': Filme.objects.all()
+    }
+    return render(request,'homefilmes.html',context)
