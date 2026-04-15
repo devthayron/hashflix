7a08ac048f6a6a8893960bd0490e9717dddb5cb9
devthayron <devthayronhiglander@gmail.com>
2026-04-15 14:51:39 -0300

    Adicionando link dinamico nos templates detailview

--- filme/urls.py
+++ filme/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import HomePageView,FilmeListView,FilmeDetailView
 
+app_name = 'filmes'
+
 urlpatterns = [
-    path('', HomePageView.as_view()),
-    path('filmes/',FilmeListView.as_view()),
-    path('filmes/<int:pk>',FilmeDetailView.as_view())
+    path('', HomePageView.as_view(), name='home'),
+    path('filmes/', FilmeListView.as_view(), name='list'),
+    path('filmes/<int:pk>/', FilmeDetailView.as_view(), name='detail')
 
 ]
