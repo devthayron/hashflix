8e96876a0d0822239d277928fea9896acc8d4816
devthayron <devthayronhiglander@gmail.com>
2026-04-16 19:59:56 -0300

    removendo o context.py e adicionando no service.py utilizandono list.views os contexts criados

--- filme/service.py
+++ filme/service.py
@@ -1,16 +1,16 @@
 from .models import Filme
 
 def get_filmes_relacionados(filme, limite=5):
-    """
-    Retorna filmes da mesma categoria do filme informado.
-    
-    excluindo o próprio registro e aplicando limite de retorno.
-
-    Utilizado como fonte de dados para a seção de recomendações na interface de detalhe do filme.
-    """
 
     filmes_relacionados = Filme.objects.filter(
         categoria=filme.categoria
     ).exclude(id=filme.id)[:limite]
 
     return filmes_relacionados
+
+
+def get_lista_filmes_recentes(limite=5):
+    return Filme.objects.order_by('-data_criacao')[:limite]
+
+def get_lista_filmes_em_alta(limite=5):
+    return Filme.objects.order_by('visualizacoes')[:limite]
--- filme/views.py
+++ filme/views.py
@@ -1,6 +1,6 @@
 from .models import Filme
 from django.views.generic import TemplateView,ListView,DetailView
-from .service import get_filmes_relacionados
+from .service import get_filmes_relacionados,get_lista_filmes_recentes,get_lista_filmes_em_alta
 
 class HomePageView(TemplateView):
     template_name = 'core/homepage.html'
@@ -9,6 +9,13 @@ class FilmeListView(ListView):
     template_name = 'filmes/list.html'
     model = Filme
     context_object_name = 'filmes'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+                
+        context['lista_filmes_recentes'] = get_lista_filmes_recentes()
+        context['lista_filmes_em_alta'] = get_lista_filmes_em_alta()
+        return context
 
 class FilmeDetailView(DetailView):
 
