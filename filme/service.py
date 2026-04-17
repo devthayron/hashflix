220b367c43243f5460a5bf8827c72cb7d8bee595
devthayron <devthayronhiglander@gmail.com>
2026-04-17 09:43:06 -0300

    adiciona incremento de visualizações no detalhe do filme

--- filme/service.py
+++ filme/service.py
@@ -1,4 +1,6 @@
 from .models import Filme
+from django.db.models import F
+
 
 def get_filmes_relacionados(filme, limite=5):
 
@@ -13,4 +15,8 @@ def get_lista_filmes_recentes(limite=5):
     return Filme.objects.order_by('-data_criacao')[:limite]
 
 def get_lista_filmes_em_alta(limite=5):
-    return Filme.objects.order_by('visualizacoes')[:limite]
+    return Filme.objects.order_by('-visualizacoes')[:limite]
+
+# class F → função do Django que permite fazer operações diretamente no banco de dados.
+def incrementar_visualizacoes(filme):
+    Filme.objects.filter(id=filme.id).update(visualizacoes=F('visualizacoes') + 1)
--- filme/views.py
+++ filme/views.py
@@ -1,6 +1,6 @@
 from .models import Filme
 from django.views.generic import TemplateView,ListView,DetailView
-from .service import get_filmes_relacionados,get_lista_filmes_recentes,get_lista_filmes_em_alta
+from .service import get_filmes_relacionados,get_lista_filmes_recentes,get_lista_filmes_em_alta,incrementar_visualizacoes
 
 class HomePageView(TemplateView):
     template_name = 'core/homepage.html'
@@ -12,7 +12,6 @@ class FilmeListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-                
         context['lista_filmes_recentes'] = get_lista_filmes_recentes()
         context['lista_filmes_em_alta'] = get_lista_filmes_em_alta()
         return context
@@ -23,7 +22,14 @@ class FilmeDetailView(DetailView):
     model = Filme       
     context_object_name = 'filme'
 
-    # função que serve para adicionar dados extras no template
+    def get_object(self, queryset = None):
+        """
+        Incrementa visualizações sempre que o filme é acessado.
+        """
+        filme = super().get_object(queryset)
+        incrementar_visualizacoes(filme)
+        return filme
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         filme = self.object # filme atual
@@ -31,3 +37,4 @@ class FilmeDetailView(DetailView):
         context['filmes_relacionados'] = get_filmes_relacionados(filme)
 
         return context
+
