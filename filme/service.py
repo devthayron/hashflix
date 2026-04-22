ea5595c573e0ccd05afa6fe0dc1c6740e7265a8c
devthayron <devthayronhiglander@gmail.com>
2026-04-22 11:25:19 -0300

    Editando list.html do filmes

--- filme/service.py
+++ filme/service.py
@@ -1,7 +1,7 @@
 from .models import Filme
 from django.db.models import F
 
-def get_filmes_relacionados(filme, limite=5):
+def get_filmes_relacionados(filme, limite=8):
 
     filmes_relacionados = Filme.objects.filter(
         categoria=filme.categoria
@@ -9,14 +9,13 @@ def get_filmes_relacionados(filme, limite=5):
 
     return filmes_relacionados
 
-def get_lista_filmes_recentes(limite=5):
+def get_lista_filmes_recentes(limite=8):
     return Filme.objects.order_by('-data_criacao')[:limite]
 
-def get_lista_filmes_em_alta(limite=5):
+def get_lista_filmes_em_alta(limite=8):
     return Filme.objects.order_by('-visualizacoes')[:limite]
 
-# class F → função do Django que permite fazer operações diretamente no banco de dados.
-def incrementar_visualizacoes(filme,limite=5):
-    Filme.objects.filter(id=filme.id).update(visualizacoes=F('visualizacoes') + 1)
-    return Filme.objects.order_by('-visualizacoes')[:limite]
-
+# F → executa a operação no banco (UPDATE direto)
+# evita race condition (duas requisições sobrescrevendo o mesmo valor)
+def incrementar_visualizacoes(filme):
+    return Filme.objects.filter(id=filme.id).update(visualizacoes=F('visualizacoes') + 1)
