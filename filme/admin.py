e56d107e3dfb98e1ead000f37da9c1063826a72b
devthayron <devthayronhiglander@gmail.com>
2026-04-23 17:46:46 -0300

    Adicionando campos no admin.py para model filme e episodio

--- filme/admin.py
+++ filme/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin 
 from .models import Filme,Episodio
 
-admin.site.register(Filme)
-admin.site.register(Episodio)
+@admin.register(Filme)
+class FilmeAdmin(admin.ModelAdmin):
+    list_display = ('titulo','categoria','visualizacoes','data_criacao','destaque')
+    list_filter = ('categoria',)
+    search_fields = ('titulo',)
+    ordering = ('-visualizacoes',)
+
+@admin.register(Episodio)
+class EpisodioAdmin(admin.ModelAdmin):
+    list_display = ('filme','titulo')
+    list_filter = ('filme',)
+    search_fields = ('filme',)
