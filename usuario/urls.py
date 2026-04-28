0160c344e1ce4946374784fa48738a9f9c5aeccc
devthayron <devthayronhiglander@gmail.com>
2026-04-28 16:24:42 -0300

    Editar perfil e mudar senha do usuário

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import ProfileView,RegisterView    
+from .views import ProfileView,RegisterView
+from django.urls import reverse_lazy 
 
 app_name = 'usuarios'
 
@@ -8,5 +9,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='usuarios/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='usuarios/logout.html'), name='logout'),
     path('registrar/', RegisterView.as_view(), name='register'),
-    path('perfil/', ProfileView.as_view(), name='profile'),
+    path('perfil/<int:pk>/', ProfileView.as_view(), name='profile'),
+    path('perfil/<int:pk>/alterar-senha/', auth_views.PasswordChangeView.as_view(template_name='usuarios/profile.html', success_url=reverse_lazy('filmes:list')), name='change_password'),
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView,FormView
+from django.views.generic import FormView,UpdateView
 from .models import Usuario
 from .forms import CustomUserCreationForm
 from django.urls import reverse_lazy
 
 
-class ProfileView(LoginRequiredMixin, TemplateView):
+class ProfileView(LoginRequiredMixin, UpdateView):
     template_name = 'usuarios/profile.html'
+    model = Usuario
+    fields = ['first_name', 'last_name', 'email']
+    success_url = reverse_lazy('filmes:list')
+
 
 class RegisterView(FormView):
     template_name = 'usuarios/register.html'
