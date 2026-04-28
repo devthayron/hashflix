8b7ec1071d0e5a3e60c862b737bc6b1fdd173250
devthayron <devthayronhiglander@gmail.com>
2026-04-28 15:22:50 -0300

    criar formulario personalizado e criar conta do usuario

--- usuario/views.py
+++ usuario/views.py
@@ -1,10 +1,19 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView,CreateView
+from django.views.generic import TemplateView,FormView
 from .models import Usuario
+from .forms import CustomUserCreationForm
+from django.urls import reverse_lazy
+
 
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'usuarios/profile.html'
 
-class RegisterView(TemplateView):
+class RegisterView(FormView):
     template_name = 'usuarios/register.html'
-  
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('usuarios:login')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+        
